fd7136c6ec4cb66c7dfe8e162b92cbf6167a3acf
chrisw <chrisw@soe.ucsc.edu>
2014-01-15 12:49:43 -0800

    added css file and some script files

---
+++ heatmap/scripts/drawHeatmap.js
@@ -0,0 +1,10 @@
+/**
+ * chrisw@soe.ucsc.edu
+ * 15JAN14
+ * Draw heatmaps via d3.js.
+ */
+
+// TODO onload
+window.onload = function() {
+    console.log("Page loaded. Start onload.");
+};
---
+++ heatmap/scripts/heatmapData.js
@@ -0,0 +1,9 @@
+/**
+ * chrisw@soe.ucsc.edu
+ * 15JAN14
+ * heatmapData.js contains objects meant to facilitate the drawing of heatmaps via d3.js.
+ */
+
+function heatmapData() {
+
+}
