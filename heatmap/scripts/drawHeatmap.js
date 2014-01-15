d996aa9ed579b273abde4a2225b86b8bea0ba009
chrisw <chrisw@soe.ucsc.edu>
2014-01-15 13:21:49 -0800

    comment example

--- heatmap/scripts/drawHeatmap.js
+++ heatmap/scripts/drawHeatmap.js
@@ -10,8 +10,16 @@ function example() {
         right : 0,
         bottom : 100,
         left : 30
-    }, width = 960 - margin.left - margin.right, height = 430 - margin.top - margin.bottom, gridSize = Math.floor(width / 24), legendElementWidth = gridSize * 2, buckets = 9, colors = ["#ffffd9", "#edf8b1", "#c7e9b4", "#7fcdbb", "#41b6c4", "#1d91c0", "#225ea8", "#253494", "#081d58"], // alternatively colorbrewer.YlGnBu[9]
-    days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"], times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
+    };
+    var width = 960 - margin.left - margin.right;
+    var height = 430 - margin.top - margin.bottom;
+    var gridSize = Math.floor(width / 24);
+    var legendElementWidth = gridSize * 2;
+    var buckets = 9;
+    var colors = ["#ffffd9", "#edf8b1", "#c7e9b4", "#7fcdbb", "#41b6c4", "#1d91c0", "#225ea8", "#253494", "#081d58"];
+    // alternatively colorbrewer.YlGnBu[9];
+    var days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+    var times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
 
     d3.tsv("heatmap/data/data.tsv", function(d) {
         return {
@@ -20,12 +28,15 @@ function example() {
             value : +d.value
         };
     }, function(error, data) {
+        // color scale
         var colorScale = d3.scale.quantile().domain([0, buckets - 1, d3.max(data, function(d) {
             return d.value;
         })]).range(colors);
 
+        // SVG canvas
         var svg = d3.select("#example").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+        // y-axis labels
         var dayLabels = svg.selectAll(".dayLabel").data(days).enter().append("text").text(function(d) {
             return d;
         }).attr("x", 0).attr("y", function(d, i) {
@@ -34,6 +45,7 @@ function example() {
             return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis");
         });
 
+        // x-axis labels
         var timeLabels = svg.selectAll(".timeLabel").data(times).enter().append("text").text(function(d) {
             return d;
         }).attr("x", function(d, i) {
@@ -42,30 +54,36 @@ function example() {
             return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis");
         });
 
+        // heatmap SVG elements
         var heatMap = svg.selectAll(".hour").data(data).enter().append("rect").attr("x", function(d) {
             return (d.hour - 1) * gridSize;
         }).attr("y", function(d) {
             return (d.day - 1) * gridSize;
         }).attr("rx", 4).attr("ry", 4).attr("class", "hour bordered").attr("width", gridSize).attr("height", gridSize).style("fill", colors[0]);
 
+        // heatmap transition/animation
         heatMap.transition().duration(1000).style("fill", function(d) {
             return colorScale(d.value);
         });
 
+        // heatmap titles
         heatMap.append("title").text(function(d) {
             return d.value;
         });
 
+        // legend SVG element
         var legend = svg.selectAll(".legend").data([0].concat(colorScale.quantiles()), function(d) {
             return d;
         }).enter().append("g").attr("class", "legend");
 
+        // legend rectangles
         legend.append("rect").attr("x", function(d, i) {
             return legendElementWidth * i;
         }).attr("y", height).attr("width", legendElementWidth).attr("height", gridSize / 2).style("fill", function(d, i) {
             return colors[i];
         });
 
+        // legend text
         legend.append("text").attr("class", "mono").text(function(d) {
             return "≥ " + Math.round(d);
         }).attr("x", function(d, i) {
