07bebcd2d55175f3a1a3554e0c9d2502f8c7a814
chrisw <chrisw@soe.ucsc.edu>
2014-01-15 16:25:35 -0800

    pretty much recreated the example

--- heatmap/scripts/drawHeatmap.js
+++ heatmap/scripts/drawHeatmap.js
@@ -135,7 +135,7 @@ function setHeatmapData(url) {
         var colors = ["#ffffd9", "#edf8b1", "#c7e9b4", "#7fcdbb", "#41b6c4", "#1d91c0", "#225ea8", "#253494", "#081d58"];
         var buckets = colors.length;
         var colorScale = d3.scale.quantile().domain([0, buckets - 1, d3.max(dataObj.getAllValues(), function(d) {
-            return d;
+            return parseFloat(d);
         })]).range(colors);
         console.log(colors);
 
